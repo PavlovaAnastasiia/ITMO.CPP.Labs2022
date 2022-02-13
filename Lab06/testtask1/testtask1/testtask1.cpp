7eb623c7e5712e34777d9874739effe4e7c14bed
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2022-02-14 00:06:05 +0300

    .

---
+++ Lab06/testtask1/testtask1/testtask1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    /*ofstream outf("SomeText.txt");
+
+    if (!outf)
+    {
+        cerr << "Unable to open file!" << endl;
+    }
+    outf << "See line #1!" << endl;
+    outf << "See line #2!" << endl;
+
+    ifstream inf("SomeText.txt");
+	if (!inf)
+	{
+		cerr << "Unable to open file!" << endl;
+	}
+
+	while (inf)
+	{
+		string strInput;
+		getline(inf, strInput);
+		cout << strInput << endl;
+	}*/
+
+    const int N = 128;
+    char text[N];
+    cout << "Type here: \n";
+    cin.getline(text, N);
+    ofstream fout("haiku.txt"); 
+    fout << text; 
+    fout.close(); 
+  
+    return 0;
+}
