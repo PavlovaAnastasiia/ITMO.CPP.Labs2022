df3b3ddb11048286c6b5086aef6b5c4250d2e936
Anastasiya Pavlova <AnastasiyaPavlova@itcenterifmo.onmicrosoft.com>
2022-02-14 13:40:10 +0300

    .

--- Lab07/task4/task4/task4.cpp
+++ Lab07/task4/task4/task4.cpp
@@ -5,7 +5,23 @@
 
 using namespace std;
 
-void printTupleOfThree(tuple<string, int, double>);
+//typedef tuple<string, int, double> Tuple; //task5
+using Tuple = tuple<string, int, double>;
+
+//void printTupleOfThree(tuple<string, int, double> t)
+void printTupleOfThree(Tuple t)
+{
+	cout << "("
+		<< get<0>(t) << ", "
+		<< get<1>(t) << ", "
+		<< get<2>(t) << ")" << endl;
+}
+
+Tuple funtup(string s, int a, double d)
+{
+	s.append("!");
+	return make_tuple(s, a, d * 10);
+}
 
 int main()
 {
@@ -16,14 +32,10 @@ int main()
 	auto t0 = make_tuple(v1[0], v2[0], v3[0]);
 	printTupleOfThree(t0);
 
+	auto t1 = funtup(v1[1], v2[1], v3[1]);
+	printTupleOfThree(t1);
+
 	return 0;
 }
 
-void printTupleOfThree(tuple<string, int, double> t)
-{
-	cout << "("
-		<< get<0>(t) << ", "
-		<< get<1>(t) << ", "
-		<< get<2>(t) << ")" << endl;
-}
 
